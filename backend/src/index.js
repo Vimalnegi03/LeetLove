5343001681d8f35146cfc47a08987918aa233ab1
Vimal Negi <109637181+Vimalnegi03@users.noreply.github.com>
2025-07-20 16:42:25 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import cookieParser from 'cookie-parser'
 dotenv.config()
 const app=express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://leetlove-1.onrender.com",
     credentials: true
 }))
 const PORT=process.env.PORT || 8080
