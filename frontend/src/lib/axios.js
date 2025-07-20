a34fccb7e37c39d9853f811ccf6aabc61c745bb3
Vimal Negi <109637181+Vimalnegi03@users.noreply.github.com>
2025-07-20 16:45:56 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance=axios.create({
-    baseURL:import.meta.env.MODE==='development'?"http://localhost:8080/api/v1":"https://leetlove.onrender.com",
+    baseURL:import.meta.env.MODE==='development'?"http://localhost:8080/api/v1":"https://leetlove.onrender.com/api/v1",
     withCredentials:true,//to include cookies and headers
 
 })
